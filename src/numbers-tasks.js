5894a5ee5dd11646e08d1ce21ca7370f62e270f2
Oksana Bebesh <oksana.bebesh1984@gmail.com>
2023-12-23 16:42:38 +0100

    feat: add one task anwer

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -648,8 +648,12 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  for (let i = 1; i <= number; i += 1) {
+    count = i % 2 === 0 ? count + 1 : count + 0;
+  }
+  return count;
 }
 
 module.exports = {
